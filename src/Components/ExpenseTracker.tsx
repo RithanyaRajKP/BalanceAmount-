171407e9d6605a30d7ee7cc7b6b99171f03f5a58
RithanyaRajKP <kprithanyaraj@gmail.com>
2023-07-19 12:10:34 +0530

    completed the transaction details display

--- src/Components/ExpenseTracker.tsx
+++ src/Components/ExpenseTracker.tsx
@@ -9,16 +9,15 @@ interface Expense {
 
 const ExpenseTracker: React.FC = () => {
   const [expenses, setExpenses] = useState<Expense[]>([]);
-  const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
   const [error, setError] = useState("");
   const [totalAmount, setTotalAmount] = useState(0);
 
-  const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAmountChange = (event:any) => {
     setAmount(event.target.value);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
 
     if (!amount) {
@@ -30,7 +29,7 @@ const ExpenseTracker: React.FC = () => {
       id: Date.now(),
       amount: +amount,
       timestamp: new Date(),
-      action: "added",
+      action: "add",
     };
 
     setExpenses([...expenses, newExpense]);
@@ -42,7 +41,8 @@ const ExpenseTracker: React.FC = () => {
   };
 
   const handleRemove = () => {
-    if (!amount) {
+    if (parseInt(amount) <= totalAmount) {
+      if (!amount) {
       setError("Please enter amount.");
       return;
     }
@@ -50,56 +50,73 @@ const ExpenseTracker: React.FC = () => {
       id: Date.now(),
       amount: +amount,
       timestamp: new Date(),
-      action: "removed",
+      action: "remove",
     };
     setExpenses([...expenses, newRExpense]);
 
     setTotalAmount(totalAmount - newRExpense.amount);
     setAmount("");
+    }
+    else{
+       setError("Please enter valid amount.");
+       return;
+    }
+    
   };
 
   return (
-    <div>
-      <div>
-        <h2>Enter Expense Details</h2>
-       
-
-        <div className="border-4">
-             <h2>Balance: {totalAmount}</h2>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="expense-amount">Amount:</label>
+    <div className="max-w-lg my-0 mx-auto flex flex-col justify-center h-screen pt-12">
+      <div className="h-full">
+        <div className="h-20 flex justify-around items-center">
+          <h2 className="text-3xl font-bold text-center">
+            Expense Tracker - Basic
+          </h2>
+        </div>
+        <div className="border-2 h-36 mb-5">
+          <h2 className="text-lg font-medium text-center pt-2.5">
+            Balance: {totalAmount}
+          </h2>
+          <form onSubmit={handleSubmit} className="text-center">
             <input
               type="number"
               id="expense-amount"
               value={amount}
               onChange={handleAmountChange}
               required
+              className="border-2 border-slate-200 rounded-md my-1 outline-0"
             />
             <br />
-            <button type="submit">Add</button>
+            <button
+              className="text-black bg-slate-300 hover:bg-gradient-to-br focus:outline-none font-medium rounded-lg text-sm px-5 py-1 text-center
+              cursor-pointer mr-2 mb-2 outline-none"
+              type="submit"
+            >
+              Add
+            </button>
             <button
               type="button"
               onClick={handleRemove}
               disabled={expenses.length === 0}
+              className="text-black bg-slate-300 hover:bg-gradient-to-br focus:outline-none font-medium rounded-lg text-sm px-5 py-1 text-center
+              cursor-pointer mr-2 mb-2"
             >
               Remove
             </button>
             {error && <p>{error}</p>}
           </form>
         </div>
-      </div>
-
-      <div>
-        <h2>Previously Entered Details</h2>
-        <ul>
-          {expenses.map((expense) => (
-            <li key={expense.id}>
-              Date:{" "}
-              {expense.timestamp ? expense.timestamp.toISOString() : "N/A"} -{" "}
-              <strong>{expense.amount} </strong> - {expense.action}
-            </li>
-          ))}
-        </ul>
+        <div className="h-2/5 border-2 text-lg font-medium px-4 py-6">
+          <h2>Transactions :</h2>
+          <ul>
+            {expenses.map((expense) => (
+              <li key={expense.id}>
+                Date:{" "}
+                {expense.timestamp ? expense.timestamp.toISOString() : "N/A"} -{" "}
+                <strong>{expense.amount} </strong> - {expense.action}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
